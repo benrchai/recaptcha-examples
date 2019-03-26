da9c26137f1676416120b3aeb3956127fdc3095a
Ben Ritthichai <ben.ritthichai@auspost.com.au>
2019-03-26 12:47:42 +1100

    Remove bot impl

--- bot/bot.js
+++ bot/bot.js
@@ -1,10 +1,5 @@
 module.exports = {
   'Bot': function(browser) {
-    browser.url('https://ptest.npe.auspost.com.au/mypost/track/#/search')
-      .waitForElementVisible('.list-page-top-banner')
-      .setValue('input[type=text]', '11223344')
-      .pause(100)
-      .click('.btn-primary')
-      .waitForElementVisible('.nickname-label-text')
+    // TO DO
   }
 }
